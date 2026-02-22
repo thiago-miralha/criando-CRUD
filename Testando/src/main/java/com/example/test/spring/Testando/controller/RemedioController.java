2ea024e4b2cdf88bebace6e4021f20251fb03b63
thiago-miralha <thiagomessiasmiralha@gmail.com>
2026-02-22 17:44:30 -0400

    conectando com o banco de dados

--- Testando/src/main/java/com/example/test/spring/Testando/controller/RemedioController.java
+++ Testando/src/main/java/com/example/test/spring/Testando/controller/RemedioController.java
@@ -8,17 +8,15 @@ import com.example.test.spring.Testando.remedio.DadosCadastroRemedio;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
 @RequestMapping("/remedio")
 public class RemedioController {
 
-@PostMapping
-public void cadastrar(@RequestBody DadosCadastroRemedio dados){
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroRemedio dados) {
 
-    System.out.println(dados);
+        System.out.println(dados);
 
-} 
-
+    }
 
 }
