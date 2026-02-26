67e83462877a3ece1c9a8d64ed85471815912cc9
thiago-miralha <thiagomessiasmiralha@gmail.com>
2026-02-25 20:55:59 -0400

    criando o delete permanente do remedio

--- Testando/src/main/java/com/example/test/spring/Testando/controller/RemedioController.java
+++ Testando/src/main/java/com/example/test/spring/Testando/controller/RemedioController.java
@@ -17,7 +17,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
@@ -55,4 +57,11 @@ public class RemedioController {
     }
 
   
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir (@PathVariable Long id) {
+
+        repository.deleteById(id);
+    }
 }
